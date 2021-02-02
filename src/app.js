e88029443de9070bafcc5f5680c7e4c92fed1e40
Mateus <mateusito10@gmail.com>
2021-02-02 17:58:37 -0300

    🚀 First Commit

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const { default: Vue } = require("vue");
+
+new Vue({
+    el: '#app',
+    data: {
+        todoList: [
+            { text: 'Fazer o header'            , done: false },
+            { text: 'Fazer uma seção dos todos' , done: false },
+            { text: 'Form para novos todos'     , done: false },
+            { text: 'Adicionar todo'            , done: false },
+            { text: 'Deletar todo'              , done: false },
+            { text: 'Completar todo'            , done: false },
+            { text: 'Paleta de cores'           , done: false },
+            { text: 'Criar os estilos'          , done: false },
+            { text: 'Filtrar todos'             , done: false },
+            { text: 'Validar input'             , done: false },
+            { text: 'Firebase / Firestore'      , done: false },
+        ]
+    }
+})
